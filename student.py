3b5f30b6583764098b3b6e93dbffc7ba182a0109
ovqnshh <vanshlikesminecraft@gmail.com>
2026-07-19 15:51:36 +0530

    Fixed GitHub Actions workflow

--- student.py
+++ student.py
@@ -2,7 +2,10 @@ students = {}
 
 
 def add_student(student_id, name):
-    return False
+    if student_id in students:
+        return False
+    students[student_id] = name
+    return True
 
 
 def remove_student(student_id):
